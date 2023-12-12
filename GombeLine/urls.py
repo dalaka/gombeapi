c53d7092a8dcf776616181b8b3b8e6017d2156d4
David <dalaka@aims.ac.tz>
2023-12-12 09:35:02 +0000

    second5

--- GombeLine/urls.py
+++ GombeLine/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
-from django.urls import re_path
-from rest_framework import permissions
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
